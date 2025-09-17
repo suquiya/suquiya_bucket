c974bf6a8f60d39ebd02032b47c2ddea9ef22c00
suquiya <22910980+suquiya@users.noreply.github.com>
2025-09-17 12:13:28 +0900

    feat: add gen_maifest.ts

---
+++ scripts/tool/ufh.ts
@@ -0,0 +1,24 @@
+import { encodeHex } from "@std/encoding/hex";
+
+if(import.meta.main){
+    for(let i = 0; i < Deno.args.length; i++){
+        const url = Deno.args[i];
+        if(url.startsWith("http://") || url.startsWith("https://")){
+            await calc_url_body_hash(Deno.args[i]);
+        }
+    }
+}
+
+
+
+async function calc_url_body_hash(url: string){
+    const res = await fetch(url);
+
+    const body = await res.arrayBuffer();
+
+    const hashBuffer = await crypto.subtle.digest("SHA-256", body);
+
+    const hash = encodeHex(hashBuffer);
+
+    console.log(`${url} => ${hash}`);
+}
